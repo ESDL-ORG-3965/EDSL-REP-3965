d913d3b143aaa7347427379ee5161e97f54f1878
DikshaYadav <diksha.yadav@cumminscollege.in>
2014-08-05 15:25:23 +0530

    Update iPercent.cpp
    Added add function to code

--- iPercent.cpp
+++ iPercent.cpp
@@ -3,7 +3,13 @@ using namespace std;
 int main()
 {
   int num1,num2,res;
-  res = num1+num2;
+  res = add(num1,num2);
   cout<<"Addition is : "<<res;
   return 0;
+}
+int add(int num1,int num2)
+{
+  int res;
+  res = num1 + num2 ;
+  return res;
 }
