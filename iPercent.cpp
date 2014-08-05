d165e3aab19753eac4de3c3a630ca44b9565c851
DikshaYadav <diksha.yadav@cumminscollege.in>
2014-08-05 15:47:59 +0530

    Update iPercent.cpp
    added multiplication function

--- iPercent.cpp
+++ iPercent.cpp
@@ -7,7 +7,10 @@ int main()
   cout<<"Addition is : "<<res;
   res1 = num1- num2;
   cout<<"Subtraction is : "<<res1;
+  res3=num1*num2;
+  cout<<"multiplication is:"<<res3;
   return 0;
+  
 }
 int add(int num1,int num2)
 {
